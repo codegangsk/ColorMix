65995972e5fedac690f655b0bd98f82b5adc4c31
Sophie Kim <kjhj0604@gmail.com>
2020-07-06 03:11:29 -0700

    Initial Commit

---
+++ ColorMix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorMix
+//
+//  Created by Sophie Kim on 2020/07/06.
+//  Copyright © 2020 Sophie Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
